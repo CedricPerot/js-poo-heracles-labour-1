65e08bd926e318aacfa62d650d12e68351e28b20
CedricPerot <122732802+CedricPerot@users.noreply.github.com>
2023-03-24 10:22:01 +0100

    premiere version

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // First Labour : Heracles vs Nemean Lion
-// use your Figher class here
+// use your Fighter class here
+const Fighter = require('./src/Fighter.js');
+const Heracles = new Fighter("🧔 Heracles", 20 , 6);
+const NemeanLion = new Fighter("🦁 Nemean Lion", 11, 16);
+
+while (Heracles.life > 0 && NemeanLion.life > 0) {
+        console.log(Heracles.fight(NemeanLion), Heracles, NemeanLion);
+        console.log(NemeanLion.fight(Heracles), Heracles, NemeanLion);
+}
+
+
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,27 @@
 /* Fighter class definition */
+const maxLife = 100;
+
+module.exports = class Fighter {
+    constructor (name, strength, dexterity){
+        this.name = name,
+        this.strength = strength,
+        this.dexterity = dexterity,
+        this.life = maxLife;
+    }
+    fight(enemy) {
+        const attack = Math.floor((Math.random() * this.strength) + 1);
+        if (attack > enemy.dexterity) {
+            const damages = attack - enemy.dexterity;
+            if (damages > enemy.life) {
+                return enemy.life = 0;
+            } 
+            else {
+                return enemy.life -= damages;
+            }
+        } 
+        else {
+            return enemy.life;
+        } 
+    }
+}
+
